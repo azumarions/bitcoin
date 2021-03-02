959a9e9e261aeb3304a69774dcfdd10c9db74b8f
azuma <azuma20011224@gmail.com>
2021-03-02 21:07:47 +0900

    finish bitflyer api function

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"bitcoin/bitflyer"
 	"bitcoin/config"
 	"bitcoin/utils"
-	"log"
+	"fmt"
+	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+
+	tickerChannel := make(chan bitflyer.Ticker)
+	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
+	for ticker := range tickerChannel {
+		fmt.Println(ticker)
+		fmt.Println(ticker.GetMidPrice())
+		fmt.Println(ticker.DateTime())
+		fmt.Println(ticker.TruncateDateTime(time.Second))
+		fmt.Println(ticker.TruncateDateTime(time.Minute))
+		fmt.Println(ticker.TruncateDateTime(time.Hour))
+	}
+
 }
