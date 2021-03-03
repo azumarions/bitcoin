2148db5b9af76676d36896a2e0bdec500ac2292f
azuma <azuma20011224@gmail.com>
2021-03-03 11:16:09 +0900

    drow candle stick chart from sqlite

--- main.go
+++ main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"bitcoin/bitflyer"
+	"bitcoin/backend/controllers"
 	"bitcoin/config"
 	"bitcoin/utils"
-	"fmt"
-	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-
-	tickerChannel := make(chan bitflyer.Ticker)
-	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
-	for ticker := range tickerChannel {
-		fmt.Println(ticker)
-		fmt.Println(ticker.GetMidPrice())
-		fmt.Println(ticker.DateTime())
-		fmt.Println(ticker.TruncateDateTime(time.Second))
-		fmt.Println(ticker.TruncateDateTime(time.Minute))
-		fmt.Println(ticker.TruncateDateTime(time.Hour))
-	}
-
+	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }
