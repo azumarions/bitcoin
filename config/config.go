ced16c2137e2647f23123d23d90bc9f19ec8ea5b
azuma <azuma20011224@gmail.com>
2021-03-08 19:09:55 +0900

    create signal_events and set backtest

--- config/config.go
+++ config/config.go
@@ -19,6 +19,12 @@ type ConfigList struct {
 	DbName        string
 	SQLDriver     string
 	Port          int
+
+	BackTest         bool
+	UsePercent       float64
+	DataLimit        int
+	StopLimitPercent float64
+	NumRanking       int
 }
 
 var Config ConfigList
@@ -37,14 +43,19 @@ func init() {
 	}
 
 	Config = ConfigList{
-		ApiKey:        cfg.Section("bitflyer").Key("api_key").String(),
-		ApiSecret:     cfg.Section("bitflyer").Key("api_secret").String(),
-		LogFile:       cfg.Section("trading").Key("log_file").String(),
-		ProductCode:   cfg.Section("trading").Key("product_code").String(),
-		Durations:     durations,
-		TradeDuration: durations[cfg.Section("trading").Key("trade_duration").String()],
-		DbName:        cfg.Section("db").Key("name").String(),
-		SQLDriver:     cfg.Section("db").Key("driver").String(),
-		Port:          cfg.Section("web").Key("port").MustInt(),
+		ApiKey:           cfg.Section("bitflyer").Key("api_key").String(),
+		ApiSecret:        cfg.Section("bitflyer").Key("api_secret").String(),
+		LogFile:          cfg.Section("trading").Key("log_file").String(),
+		ProductCode:      cfg.Section("trading").Key("product_code").String(),
+		Durations:        durations,
+		TradeDuration:    durations[cfg.Section("trading").Key("trade_duration").String()],
+		DbName:           cfg.Section("db").Key("name").String(),
+		SQLDriver:        cfg.Section("db").Key("driver").String(),
+		Port:             cfg.Section("web").Key("port").MustInt(),
+		BackTest:         cfg.Section("trading").Key("back_test").MustBool(),
+		UsePercent:       cfg.Section("trading").Key("use_percent").MustFloat64(),
+		DataLimit:        cfg.Section("trading").Key("data_limit").MustInt(),
+		StopLimitPercent: cfg.Section("trading").Key("stop_limit_percent").MustFloat64(),
+		NumRanking:       cfg.Section("trading").Key("num_ranking").MustInt(),
 	}
 }
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+// import (
+// 	"bitcoin/backend/models"
+// 	"fmt"
+// 	"time"
+// )
+
 import (
 	"bitcoin/backend/controllers"
 	"bitcoin/config"
@@ -11,3 +17,16 @@ func main() {
 	controllers.StreamIngestionData()
 	controllers.StartWebServer()
 }
+
+// func main() {
+// 	s := models.NewSignalEvents()
+// 	df, _ := models.GetAllCandle("BTC_JPY", time.Minute, 10)
+// 	c1 := df.Candles[0]
+// 	c2 := df.Candles[5]
+// 	s.Buy("BTC_JPY", c1.Time.UTC(), c1.Close, 1.0, true)
+// 	s.Sell("BTC_JPY", c2.Time.UTC(), c2.Close, 1.0, true)
+// 	fmt.Println(models.GetSignalEventsByCount(1))
+// 	fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
+// 	fmt.Println(s.CollectAfter(time.Now().UTC()))
+// 	fmt.Println(s.CollectAfter(c1.Time))
+// }
