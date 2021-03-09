e8c9f70324ec3fa16c7929657827afc7c23ec658
azuma <azuma20011224@gmail.com>
2021-03-09 21:31:43 +0900

    backtest BBands

--- backend/controllers/webserver.go
+++ backend/controllers/webserver.go
@@ -185,10 +185,32 @@ func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
 		df.AddHv(period3)
 	}
 
+	// events := r.URL.Query().Get("events")
+	// if events != "" {
+	// 	if config.Config.BackTest {
+	// 		p, p1, p2 := df.OptimizeEma()
+	// 		log.Println(p, p1, p2)
+	// 		if p > 0 {
+	// 			df.Events = df.BackTestEma(p1, p2)
+	// 		}
+	// 	} else {
+	// 		firstTime := df.Candles[0].Time
+	// 		df.AddEvents(firstTime)
+	// 	}
+	// }
+
 	events := r.URL.Query().Get("events")
 	if events != "" {
-		firstTime := df.Candles[0].Time
-		df.AddEvents(firstTime)
+		if config.Config.BackTest {
+			performance, p1, p2 := df.OptimizeBb()
+			log.Println(performance, p1, p2)
+			if performance > 0 {
+				df.Events = df.BackTestBb(p1, p2)
+			}
+		} else {
+			firstTime := df.Candles[0].Time
+			df.AddEvents(firstTime)
+		}
 	}
 
 	js, err := json.Marshal(df)
--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 	"bitcoin/backend/controllers"
 	"bitcoin/config"
 	"bitcoin/utils"
+	"log"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	controllers.StreamIngestionData()
-	controllers.StartWebServer()
+	log.Println(controllers.StartWebServer())
 }
 
 // func main() {
